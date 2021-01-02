2d03451761a95980cfd9305e88dcf45b2e5221e5
Prateek <prateekvermasdg007@gmail.com>
2021-01-02 11:11:11 +0530

    Updates

---
+++ precache-manifest.a4a58ffda73c58d2c93fc488cff7ec7c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337c10c5fcfd52a7c3ea1e3c085c58f7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3c329ab5652fc0c1944",
+    "url": "/portfolio/static/css/2.fc88e6b0.chunk.css"
+  },
+  {
+    "revision": "e24aa63eca9041e36bb2",
+    "url": "/portfolio/static/css/main.be426c47.chunk.css"
+  },
+  {
+    "revision": "f3c329ab5652fc0c1944",
+    "url": "/portfolio/static/js/2.44687202.chunk.js"
+  },
+  {
+    "revision": "e24aa63eca9041e36bb2",
+    "url": "/portfolio/static/js/main.4113416e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/portfolio/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/portfolio/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "13466dafc03290888f01582413d1b7d9",
+    "url": "/portfolio/static/media/myAura.13466daf.jpg"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/portfolio/static/media/todolist.ac82d97d.png"
+  }
+]);
